dc30842b25f3ccad35b1bed59bf00602aae419fc
99ru <yan.99ru@gmail.com>
2023-06-14 11:41:16 +0200

    added Nav & form

---
+++ src/components/NewTweet.tsx
@@ -0,0 +1,12 @@
+export function NewTweet() {
+  return (
+    <form className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+      <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+      placeholder="What's happening?!">
+      </textarea>
+      </div>
+      <button>btn</button>
+    </form>
+  );
+}
